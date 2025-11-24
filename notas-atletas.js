eff2f6a2521a6290d044eaf01e44f2dfc10245cd
Sean Araújo <seanaraujo@gmail.com>
2025-11-24 18:56:12 -0300

    Notas Atletas

---
+++ notas-atletas.js
@@ -0,0 +1,44 @@
+// javascript
+
+let atletas = [
+    {
+        nome: "Cesar Abascal",
+        notas: [10, 9.34, 8.42, 10, 7.88]
+    },
+    {
+        nome: "Fernando Puntel",
+        notas: [8, 10, 10, 7, 9.33]
+    },
+    {
+        nome: "Daiane Jelinsky",
+        notas: [7, 10, 9.5, 9.5, 8]
+    },
+    {
+        nome: "Bruno Castro",
+        notas: [10, 10, 10, 9, 9.5]
+    }
+];
+
+function exibirResultados(atletas) {
+    atletas.forEach(function (atleta) {
+        let notasOrdenadas = atleta.notas.slice().sort(function (a, b) {
+            return a - b;
+        });
+
+        let notasComputadas = notasOrdenadas.slice(1, notasOrdenadas.length - 1);
+
+        let somaNotas = notasComputadas.reduce(function (acc, nota) {
+            return acc + nota;
+        }, 0);
+
+        let mediaValida = somaNotas / notasComputadas.length;
+
+        console.log(`Atleta: ${atleta.nome}`);
+        console.log(`Notas Obtidas: ${atleta.notas.join(", ")}`);
+        console.log(`Média Válida: ${parseFloat(mediaValida.toFixed(6))}`);
+        console.log();
+
+    });
+};
+
+exibirResultados(atletas);
